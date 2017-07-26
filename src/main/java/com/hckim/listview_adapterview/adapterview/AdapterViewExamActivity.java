7b5fed2bdb271c6fbbdda9d7781d7f588c0e317d
Hongchul Kim <gsjune@naver.com>
2017-07-26 12:49:42 +0900

    ListView 가져오기

--- src/main/java/com/hckim/listview_adapterview/adapterview/AdapterViewExamActivity.java
+++ src/main/java/com/hckim/listview_adapterview/adapterview/AdapterViewExamActivity.java
@@ -2,6 +2,7 @@ package com.hckim.listview_adapterview.adapterview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.hckim.listview_adapterview.R;
 
@@ -11,5 +12,7 @@ public class AdapterViewExamActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adapter_view_exam);
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
     }
 }
