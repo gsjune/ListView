1b52bed7fe99055a780b5d6d860239110824f907
Hongchul Kim <gsjune@naver.com>
2017-07-26 13:33:31 +0900

    View/Data/Adapter set 실행

--- src/main/java/com/hckim/listview_adapterview/adapterview/AdapterViewExamActivity.java
+++ src/main/java/com/hckim/listview_adapterview/adapterview/AdapterViewExamActivity.java
@@ -2,9 +2,12 @@ package com.hckim.listview_adapterview.adapterview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.hckim.listview_adapterview.R;
+
+import java.util.ArrayList;
 
 public class AdapterViewExamActivity extends AppCompatActivity {
 
@@ -13,6 +16,19 @@ public class AdapterViewExamActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adapter_view_exam);
 
+        // View
         ListView listView = (ListView) findViewById(R.id.list_view); // (1) ListView 가져오기
+
+        // Data
+        ArrayList<String> data = new ArrayList<>(); // (2)
+        for (int i = 0; i < 100; i++) {
+            data.add("데이터 " + i);
+        }
+
+        // Adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(AdapterViewExamActivity.this,
+                android.R.layout.simple_list_item_1, data); // (3) ArrayAdapter 안쓰임
+
+        listView.setAdapter(adapter); // (4)
     }
 }
