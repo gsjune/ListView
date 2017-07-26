00683daca93852fa0644cd842a318fdd06e68348
Hongchul Kim <gsjune@naver.com>
2017-07-26 16:40:20 +0900

    String을 People로 바꾼 후 변화. ArrayAdapter는 다시 만들어야 함.

--- src/main/java/com/hckim/listview_adapterview/adapterview/AdapterViewExamActivity.java
+++ src/main/java/com/hckim/listview_adapterview/adapterview/AdapterViewExamActivity.java
@@ -20,14 +20,26 @@ public class AdapterViewExamActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.list_view); // (1) ListView 가져오기
 
         // Data
-        ArrayList<String> data = new ArrayList<>(); // (2)
+//        ArrayList<String> data = new ArrayList<>(); // (2)
+//        for (int i = 0; i < 100; i++) {
+//            data.add("데이터 " + i);
+//        }
+
+        ArrayList<People> data = new ArrayList<>(); // B(1) People로 바꾼 후 변화
         for (int i = 0; i < 100; i++) {
-            data.add("데이터 " + i);
+            int picture;
+            if (i % 2 == 0) {
+                picture = R.drawable.federer;
+            } else {
+                picture = R.mipmap.ic_launcher;
+            }
+            People people = new People("아무개 " + i, "전화번호 " + i, picture);
+            data.add(people);
         }
 
         // Adapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(AdapterViewExamActivity.this,
-                android.R.layout.simple_list_item_1, data); // (3) ArrayAdapter 잘 안쓰임
+        ArrayAdapter<People> adapter = new ArrayAdapter<People>(AdapterViewExamActivity.this,
+                android.R.layout.simple_list_item_1, data); // (3) ArrayAdapter 잘 안쓰임 B(2) 이렇게 하는 거 아님. 다시 만들어야
 
         listView.setAdapter(adapter); // (4)
     }
--- src/main/java/com/hckim/listview_adapterview/adapterview/People.java
+++ src/main/java/com/hckim/listview_adapterview/adapterview/People.java
@@ -1,8 +1,50 @@
 package com.hckim.listview_adapterview.adapterview;
 
 /**
- * Created by K on 2017-07-26.
+ * 모델 클래스
  */
 
-public class People {
+public class People { // <String>에 들어갈 데이터 타입
+    private String name;
+    private String phone;
+    private int picture;
+
+    public People(String name, String phone, int picture) { // 생성자
+        this.name = name;
+        this.phone = phone;
+        this.picture = picture;
+    }
+
+    public String getName() { // getter and setter
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getPicture() {
+        return picture;
+    }
+
+    public void setPicture(int picture) {
+        this.picture = picture;
+    }
+
+    @Override
+    public String toString() { // toString
+        return "People{" +
+                "name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", picture=" + picture +
+                '}';
+    }
 }
